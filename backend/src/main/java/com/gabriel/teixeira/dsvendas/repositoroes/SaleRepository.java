3de82561cc3752c28049cb7a8cac7b5c75042be0
Gabriel Teixeira <gteixeira539@gmail.com>
2021-11-06 11:27:32 -0300

    Group by search

--- backend/src/main/java/com/gabriel/teixeira/dsvendas/repositoroes/SaleRepository.java
+++ backend/src/main/java/com/gabriel/teixeira/dsvendas/repositoroes/SaleRepository.java
@@ -1,8 +1,20 @@
 package com.gabriel.teixeira.dsvendas.repositoroes;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gabriel.teixeira.dsvendas.dto.SaleSuccessDTO;
+import com.gabriel.teixeira.dsvendas.dto.SaleSumDTO;
 import com.gabriel.teixeira.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
+	@Query("SELECT com.gabriel.teixeira.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySaller();	
+	
+	@Query("SELECT com.gabriel.teixeira.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySaller();	
 }
--- backend/src/main/java/com/gabriel/teixeira/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/gabriel/teixeira/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.gabriel.teixeira.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gabriel.teixeira.dsvendas.dto.SaleDTO;
+import com.gabriel.teixeira.dsvendas.dto.SaleSuccessDTO;
+import com.gabriel.teixeira.dsvendas.dto.SaleSumDTO;
 import com.gabriel.teixeira.dsvendas.entities.Sale;
 import com.gabriel.teixeira.dsvendas.repositoroes.SaleRepository;
 import com.gabriel.teixeira.dsvendas.repositoroes.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySaller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySaller();
 	}
 }
